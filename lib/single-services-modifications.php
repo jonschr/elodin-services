c7e2ce3f3dfd27881c00513c9fdc14c190d5f6e9
jonschr <jonschr@gmail.com>
2020-07-16 01:08:16 -0500

    Fix an error thrown on non-Genesis themes

--- lib/single-services-modifications.php
+++ lib/single-services-modifications.php
@@ -7,7 +7,12 @@
 //* Register the widget area
 add_action( 'init', 'elodin_services_register_widget_area_below' );
 function elodin_services_register_widget_area_below() {
-    genesis_register_sidebar( array(
+    
+    //* Bail if it's not Genesis
+    if ( !function_exists( 'genesis_register_sidebar' ) )
+        return;
+    
+        genesis_register_sidebar( array(
         'id'		    => 'after-single-service',
         'name'		    => __( 'Services single footer', 'elodin-services' ),
         'description'	=> __( 'An area intended to house anything you\'d like to show on all services (perhaps a listing of all of the services for easy navigation).', 'elodin-services' ),
